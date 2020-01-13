32444b13cfd7ed490510b8fe4381a75808141f8a
王帆 <iFan@ouhans-MacBook-Pro.local>
2020-01-13 23:53:09 +0800

    解析用户列表页面时获取下一页url和其他类型的用户列表url

--- crawler/main.go
+++ crawler/main.go
@@ -7,15 +7,20 @@ import (
 )
 
 const url = "http://www.zhenai.com/zhenghun"
+const cityUrl = "http://www.zhenai.com/zhenghun/shanghai"
 
 func main() {
 	e := engine.ConcurrentEngine{
 		Scheduler:   &scheduler.QueuedScheduler{},
 		WorkerCount: 10,
 	}
+	//e.Run(engine.Request{
+	//	Url:        url,
+	//	ParserFunc: parser.ParseCityList,
+	//})
 	e.Run(engine.Request{
-		Url:        url,
-		ParserFunc: parser.ParseCityList,
+		Url:        cityUrl,
+		ParserFunc: parser.ParseCity,
 	})
 }
 
--- crawler/zhenai/parser/city.go
+++ crawler/zhenai/parser/city.go
@@ -4,22 +4,29 @@ import (
 	"regexp"
 	"studygolang/crawler/engine"
 )
+                                       // 下一页 href="http://www.zhenai.com/zhenghun/zhenjiang/2"
+var cityUrlRe = regexp.MustCompile(`href="(http://www.zhenai.com/zhenghun/[^"]+)"`)
+var cityRe = regexp.MustCompile(`<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`)
+var sexRe = regexp.MustCompile(`<td[^>]*><span class="grayL">性别：</span>([^<]+)</td>`)
 
-const cityRe = `<a href="(http://album.zhenai.com/u/[0-9]+)"[^>]*>([^<]+)</a>`
-
-const sexRe = `<td[^>]*><span class="grayL">性别：</span>([^<]+)</td>`
 
 func ParseCity(contents []byte) engine.ParseResult {
-	compile := regexp.MustCompile(cityRe)
-	matches := compile.FindAllSubmatch(contents, -1)
-
-	sexCompile := regexp.MustCompile(sexRe)
-	sexMatches := sexCompile.FindAllSubmatch(contents, -1)
-
 	result := engine.ParseResult{}
+
+	cityList := cityUrlRe.FindAllSubmatch(contents, -1)
+	for _, m := range cityList {
+		result.Requests = append(result.Requests, engine.Request{
+			Url: string(m[1]),
+			ParserFunc: ParseCity,
+		})
+	}
+
+	matches := cityRe.FindAllSubmatch(contents, -1)
+	sexMatches := sexRe.FindAllSubmatch(contents, -1)
 	for i, m := range matches {
 		user := m[2]
 		sex := sexMatches[i][1]
+
 		result.Items    = append(result.Items, "User " + string(user))
 		result.Requests = append(result.Requests, engine.Request{
 			Url:        string(m[1]),
