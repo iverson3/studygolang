7de00a1ab0f3ecb35bd78cb96b7f2eb6c5dbe4b1
wangf16 <wangf16@mingyuanyun.com>
2022-06-08 20:58:21 +0800

    computeCapacity

--- wangdis/datastruct/dict/concurrent.go
+++ wangdis/datastruct/dict/concurrent.go
@@ -1,6 +1,9 @@
 package dict
 
-import "sync"
+import (
+	"math"
+	"sync"
+)
 
 type ConcurrentDict struct {
 	table []*shard
@@ -13,3 +16,33 @@ type shard struct {
 	mu sync.RWMutex
 }
 
+func computeCapacity(param int) int {
+	if param <= 16 {
+		return 16
+	}
+	n := param - 1
+	n |= n >> 1
+	n |= n >> 2
+	n |= n >> 4
+	n |= n >> 8
+	n |= n >> 16
+	if n < 0 {
+		return math.MaxInt32
+	}
+	return n + 1
+}
+
+func MakeConcurrent(shardCount int) *ConcurrentDict {
+	shardCount = computeCapacity(shardCount)
+	table := make([]*shard, shardCount)
+	for i := 0; i < shardCount; i++ {
+		table[i] = &shard{
+			m: make(map[string]interface{}),
+		}
+	}
+	return &ConcurrentDict{
+		table:      table,
+		count:      0,
+		shardCount: shardCount,
+	}
+}
